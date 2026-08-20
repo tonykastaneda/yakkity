faf023e7612b39d11dcc3ecc373625ceed549904
tonykastaneda <tonykastaneda@gmail.com>
2026-08-20 16:27:23 -0700

    Add stable Homebrew formula

--- Formula/code-chats.rb
+++ Formula/code-chats.rb
@@ -1,6 +1,8 @@
 class CodeChats < Formula
   desc "Search, resume, and hand off conversations between coding agents"
   homepage "https://github.com/tonykastaneda/code-chats"
+  url "https://github.com/tonykastaneda/code-chats/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "a7f67830e58d895048b09725e0de512a7dcfa9730115be5fe97a8315c9d983e2"
   license "MIT"
   head "https://github.com/tonykastaneda/code-chats.git", branch: "main"
 
@@ -13,7 +15,7 @@ class CodeChats < Formula
   end
 
   test do
-    assert_match "code-chats", shell_output("#{bin}/chat --version")
+    assert_match "code-chats 0.1.0", shell_output("#{bin}/chat --version")
     assert_match "Usage: chat", shell_output("#{bin}/chat --help")
   end
 end
