ad1569e5cc12aef0fb2d469d639a0f736e14d16a
tonykastaneda <tonykastaneda@gmail.com>
2026-08-20 18:06:25 -0700

    Add stable yakkity 0.2.1 formula

--- Formula/yakkity.rb
+++ Formula/yakkity.rb
@@ -1,6 +1,8 @@
 class Yakkity < Formula
   desc "Search, resume, and hand off conversations between coding agents"
   homepage "https://yakkity.dev"
+  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "148a6c13df7c7640224c2a146914bc7b34bc031843eebdd12444426188b07a8b"
   license "MIT"
   head "https://github.com/tonykastaneda/yakkity.git", branch: "main"
 
