bc72ee7c4ab3890d145949fdb1f7f9838c7e85f8
tonykastaneda <tonykastaneda@gmail.com>
2026-08-20 17:39:43 -0700

    Add stable yakkity Homebrew formula

--- Formula/yakkity.rb
+++ Formula/yakkity.rb
@@ -1,6 +1,8 @@
 class Yakkity < Formula
   desc "Search, resume, and hand off conversations between coding agents"
   homepage "https://yakkity.dev"
+  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.2.0.tar.gz"
+  sha256 "ad2760d1d4c95723db57a56c791b76cffa2cc066f6a83907fe443bf9fd1c471f"
   license "MIT"
   head "https://github.com/tonykastaneda/yakkity.git", branch: "main"
 
