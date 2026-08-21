10311ec8776b177b57acda35315cb7dbbdc8c453
tonykastaneda <tonykastaneda@gmail.com>
2026-08-21 09:50:54 -0700

    Add stable yakkity 0.2.2 formula

--- Formula/yakkity.rb
+++ Formula/yakkity.rb
@@ -1,8 +1,8 @@
 class Yakkity < Formula
   desc "Search, resume, and hand off conversations between coding agents"
   homepage "https://yakkity.dev"
-  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.2.1.tar.gz"
-  sha256 "148a6c13df7c7640224c2a146914bc7b34bc031843eebdd12444426188b07a8b"
+  url "https://github.com/tonykastaneda/yakkity/archive/refs/tags/v0.2.2.tar.gz"
+  sha256 "263587a505e10d329556b4f971dca3c494823952827b129306d95c346e5f901d"
   license "MIT"
   head "https://github.com/tonykastaneda/yakkity.git", branch: "main"
 
@@ -15,7 +15,7 @@ class Yakkity < Formula
   end
 
   test do
-    assert_match "yakkity 0.2.1", shell_output("#{bin}/yakk --version")
+    assert_match "yakkity 0.2.2", shell_output("#{bin}/yakk --version")
     assert_match "Usage: yakk", shell_output("#{bin}/yakk --help")
   end
 end
